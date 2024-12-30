1210ef1e26828dd9dccfcaef556c43d5175d056a
AmirrezaRezaeii <amirrezarezaeijavidan@gmail.com>
2024-12-30 12:46:37 +0330

    Correct way of using middleware

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const { http } = require("msw");
+const { http, HttpResponse } = require("msw");
 const { createMiddleware } = require("@mswjs/http-middleware");
 
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res, ctx) => {
-  return res.send("Hello World!");
-});
-
 app.use(
   createMiddleware(
-    http.get("/", (req, res, ctx) => {
-      return res(ctx.text("salam"));
+    http.get("/", () => {
+      return HttpResponse.text("salam");
     })
   )
 );
+
+app.get("/", (req, res, ctx) => {
+  return res.send("Hello World!");
+});
 
 app.listen(3001, () => {
   console.log("working");
